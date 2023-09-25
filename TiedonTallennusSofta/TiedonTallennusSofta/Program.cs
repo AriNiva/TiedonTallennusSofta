bdc8d92579908dabb5a068fc3b1c97518927d6da
AriNiva <145963185+AriNiva@users.noreply.github.com>
2023-09-25 11:03:32 +0300

    Add files via upload

---
+++ TiedonTallennusSofta/TiedonTallennusSofta/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiedonTallennusSofta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Muuttujien luonnit ja alustukset
+            string tiedosto = "C:\\Temp\\Lampotila.txt";//Tiedostonimeen viittaus tapa1
+            string tiedosto2 = @"C:\Temp\Lampotila.txt";//Tiedostonimeen viittaus tapa2 @-merkillä
+            string aikaisempiArvo, uusiArvo;
+            //Luetaan tiedostosta aikaisempi lämpötila-arvo
+            if (File.Exists(tiedosto))
+            {
+                aikaisempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("Lämpötila on: {0} astetta", aikaisempiArvo);
+            }
+            else 
+            {
+                Console.WriteLine("Tervetuloa!");
+            }
+            Console.Write("Anna uusi lämpötila: ");
+            uusiArvo = Console.ReadLine();
+            //Tiedostoon kirjoittaminen
+            File.WriteAllText(tiedosto, uusiArvo);
+            Console.WriteLine("Uusi arvo talletettu!");
+            Console.ReadLine();
+        }
+    }
+}
